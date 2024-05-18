0be60b73c2755d39a6b45c24c5a278a0262491e9
NikolaBV <bulgarianmapper@gmail.com>
2024-05-18 11:29:17 +0300

    Improved: Controller structure

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,16 +9,11 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseAPIController
     {
-        public IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
