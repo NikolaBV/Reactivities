024bd7f6da25a2331f90fa9114d8ea8be285a991
NikolaBV <bulgarianmapper@gmail.com>
2024-07-18 17:44:48 +0300

    Posting and deleting added

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,12 +6,14 @@ interface Props {
   closeForm: () => void;
   activity: Activity | undefined;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityForm({
   activity: selectedActivity,
   closeForm,
   createOrEdit,
+  submitting,
 }: Props) {
   const initialState = selectedActivity ?? {
     id: "",
@@ -37,47 +39,49 @@ export default function ActivityForm({
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autocomplete="off">
-        <Form.TextArea
+        <Form.Input
           placeholder="Title"
           value={activity.title}
           name="title"
           onChange={handleInputChange}
-        ></Form.TextArea>
-        <Form.TextArea
+        ></Form.Input>
+        <Form.Input
           placeholder="Date"
+          type="date"
           value={activity.date}
           name="date"
           onChange={handleInputChange}
-        ></Form.TextArea>
+        ></Form.Input>
         <Form.TextArea
           placeholder="Description"
           value={activity.description}
           name="description"
           onChange={handleInputChange}
         ></Form.TextArea>
-        <Form.TextArea
+        <Form.Input
           placeholder="Category"
           value={activity.category}
           name="category"
           onChange={handleInputChange}
-        ></Form.TextArea>
-        <Form.TextArea
+        ></Form.Input>
+        <Form.Input
           placeholder="Location"
           value={activity.location}
           name="location"
           onChange={handleInputChange}
-        ></Form.TextArea>
-        <Form.TextArea
+        ></Form.Input>
+        <Form.Input
           placeholder="Venue"
           value={activity.venue}
           name="venue"
           onChange={handleInputChange}
-        ></Form.TextArea>
+        ></Form.Input>
         <Button
           floated="right"
           positive
           type="submit"
           content="submit"
+          loading={submitting}
         ></Button>
         <Button
           onClick={closeForm}
